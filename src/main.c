72a93b0ced93712133262f83e3b5b20c3f1d7fe4
arvs <arjunvtsuresh@gmail.com>
2025-04-23 21:39:16 +0530

    fix

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdbool.h>
 
 // wrapper
 
@@ -34,15 +34,15 @@ ssize_t getline( char **lineptr, size_t *n, FILE *stream);
 // Read Input of User
 
 void read_input(InputBuffer* input_buffer){
-	ssize_t bytes = getline(&(input_buffer->buffer), &(input_buffer->buffer_length),stdin);
+	ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length),stdin);
 
-	if (bytes <= 0){
+	if (bytes_read <= 0){
 		printf("Error reading input\n");
 		exit(EXIT_FAILURE);
 	}
 
-	input_buffer->input_length = bytes -1;
-	input_buffer->buffer[bytes - 1]=0;
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1]=0;
 }
 
 void close_input_buffer(InputBuffer* input_buffer){
@@ -52,17 +52,16 @@ void close_input_buffer(InputBuffer* input_buffer){
 
 int main(int argc , char* argv[]){
 	InputBuffer* input_buffer = new_input_buffer();
-	while(1){
+	while(true){
 		print_prompt();
 		read_input(input_buffer);
-	}
-
-	if (strcmp(input_buffer->buffer, ".exit")==0){
-		close_input_buffer(input_buffer);
-		exit(EXIT_SUCCESS);
-	}
-	else{
-	printf("Unrecognized command '%s'. \n",input_buffer->buffer);
+		if (strcmp(input_buffer->buffer, ".exit")==0){
+			close_input_buffer(input_buffer);
+			exit(EXIT_SUCCESS);
+		}
+		else{
+		printf("Unrecognized command '%s'. \n",input_buffer->buffer);
+		}
 	}
 
 }
